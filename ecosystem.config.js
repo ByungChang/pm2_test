8cd5961dec00f27029a8aa94b3ff7d63e8394aea
KangByungChang <rktqudckd94@naver.com>
2019-09-20 09:13:33 +0900

    개발서버 설치 경로 변경

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -29,7 +29,7 @@ module.exports = {
         host: [{ host: "222.107.251.211", port: "40022" }],
         ref: "origin/master", // 리모트 브랜치
         repo: "git@github.com:byungchang/pm2_test.git", // Github 프로젝트 주소
-        path: "/root/node_test/pm2_con", // 원격 서버에서 프로젝트를 생성할 위치
+        path: "/var/www/pm2_test", // 원격 서버에서 프로젝트를 생성할 위치
         // 프로젝트 배포 후 실행할 명령어
         //"post-deploy":
           //"npm install && pm2 reload ecosystem.config.js --env development"
